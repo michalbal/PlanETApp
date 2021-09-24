6160e3a7bb8ad17c8bc3f95bf431531dcd6ff801
Nataly Milman <n.pervusiak@gmail.com>
2021-09-24 23:37:56 +0300

    Ignore list (tasks and courses) and tasks filtering

--- app/src/main/java/net/planner/planetapp/planner/TasksManager.java
+++ app/src/main/java/net/planner/planetapp/planner/TasksManager.java
@@ -10,17 +10,9 @@ import java.util.LinkedList;
 public class TasksManager {
     private final String token;
     private final MoodleCommunicator connector;
+    private final DBmanager dBmanager;
     private ArrayList<String> unwantedCourseIds;
     private ArrayList<String> unwantedTaskIds;
-    private DBmanager dBmanager;
-
-    public void setUnwantedCourseIds(ArrayList<String> unwantedCourseIds) {
-        this.unwantedCourseIds = unwantedCourseIds;
-    }
-
-    public void setUnwantedTaskIds(ArrayList<String> unwantedTaskIds) {
-        this.unwantedTaskIds = unwantedTaskIds;
-    }
 
     public TasksManager(String username, String password) {
         connector = new MoodleCommunicator();
@@ -30,52 +22,47 @@ public class TasksManager {
         dBmanager.getUnwantedTasks(this);
     }
 
-    // TODO need username for dbmanager, can't use token until it's figured out (shared
-    //  preferences?)
-//    public TasksManager(String userToken) {
-//        connector = new MoodleCommunicator();
-//        token = userToken;
-//
-//        unwantedTaskIds = new ArrayList<>();
-//        unwantedCourseIds = new ArrayList<>();
-//    }
+    public void setUnwantedCourseIds(ArrayList<String> unwantedCourseIds) {
+        this.unwantedCourseIds = unwantedCourseIds;
+    }
+
+    public void setUnwantedTaskIds(ArrayList<String> unwantedTaskIds) {
+        this.unwantedTaskIds = unwantedTaskIds;
+    }
 
     public HashMap<String, String> parseMoodleCourses() {
         if (token != null && !token.equals("")) {
-            HashMap<String, String> parsedCourseNames = connector.parseFromMoodle(
-                    token, true);
-
-            for (HashMap.Entry<String, String> parsedCourseName : parsedCourseNames
-                    .entrySet()) {
-                System.out.print(parsedCourseName.getKey() + "\t-\t");
-                System.out.println(parsedCourseName.getValue());
-            }
+            HashMap<String, String> parsedCourseNames = connector.parseFromMoodle(token, true);
             return parsedCourseNames;
         }
         return null;
     }
 
-    public HashMap<String, LinkedList<PlannerTask>> parseMoodleTasks() {
+    public LinkedList<PlannerTask> parseMoodleTasks(long currentTime) {
         // TODO normal moodle as an option as well
+        LinkedList<PlannerTask> filteredTasks = new LinkedList<>();
         if (token != null && !token.equals("")) {
             HashMap<String, LinkedList<PlannerTask>> parsedAssignments = connector.parseFromMoodle(
                     token, false);
 
             for (HashMap.Entry<String, LinkedList<PlannerTask>> parsedAssignment : parsedAssignments
                     .entrySet()) {
-                System.out.print(parsedAssignment.getKey() + "\t-\t");
-                System.out.println(parsedAssignment.getValue());
-                if (unwantedCourseIds.contains(parsedAssignment.getKey())){
-                    parsedAssignments.remove(parsedAssignment.getKey());
+                if (unwantedCourseIds.contains(parsedAssignment.getKey())) {
+                    continue;
+                }
+
+                for (PlannerTask task : parsedAssignment.getValue()) {
+                    if (!unwantedTaskIds.contains(task.getMoodleId()) &&
+                        task.getDeadline() > currentTime) {
+                        filteredTasks.add(task);
+                    }
                 }
             }
-            // TODO remove unwanted courses and tasks
-            return parsedAssignments;
         }
-        return null;
+        return filteredTasks;
     }
 
-    public LinkedList<PlannerEvent> planSchedule(LinkedList<PlannerTask> plannerTasks){
+    public LinkedList<PlannerEvent> planSchedule(LinkedList<PlannerTask> plannerTasks) {
         dBmanager.writeAcceptedTasks(plannerTasks);
 
         LinkedList<PlannerEvent> subtasks = null;
@@ -83,29 +70,41 @@ public class TasksManager {
         return subtasks;
     }
 
-    public void processUserAcceptedSubtasks(LinkedList<PlannerEvent> acceptedEvents){
+    public void processUserAcceptedSubtasks(LinkedList<PlannerEvent> acceptedEvents) {
         //TODO write to GC, get IDs and update them in the events (will be used in db)
 
         // write to db
         dBmanager.writeNewSubtasks(acceptedEvents);
     }
 
-    public void addTaskToUnwanted(String moodleTaskId){
+    public void addTasksToUnwanted(LinkedList<PlannerTask> tasks){
+        for (PlannerTask task : tasks){
+            addTaskToUnwanted(task.getMoodleId());
+        }
+    }
+
+    public void addCoursesToUnwanted(LinkedList<String> courseIds){
+        for (String course : courseIds){
+            addTaskToUnwanted(course);
+        }
+    }
+
+    public void addTaskToUnwanted(String moodleTaskId) {
         unwantedTaskIds.add(moodleTaskId);
         dBmanager.addUnwantedTask(moodleTaskId);
     }
 
-    public void removeTaskFromUnwanted(String moodleTaskId){
+    public void removeTaskFromUnwanted(String moodleTaskId) {
         unwantedTaskIds.remove(moodleTaskId);
         dBmanager.removeUnwantedTask(moodleTaskId);
     }
 
-    public void addCourseToUnwanted(String courseId){
+    public void addCourseToUnwanted(String courseId) {
         unwantedCourseIds.add(courseId);
         dBmanager.addUnwantedCourse(courseId);
     }
 
-    public void removeCourseFromUnwanted(String courseId){
+    public void removeCourseFromUnwanted(String courseId) {
         unwantedCourseIds.remove(courseId);
         dBmanager.removeUnwantedCourse(courseId);
     }
